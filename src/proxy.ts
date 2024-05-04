184337cb88dee85822eb75e41fd0e6a0e92544e5
Vicfou <victormorel1998@gmail.com>
2024-05-04 22:38:20 +0200

    push

--- src/proxy.ts
+++ src/proxy.ts
@@ -246,8 +246,8 @@ function getHandler(options: CorsProxyOptions, proxy: any): (req: any, res: any)
                 res.end('The URL is invalid: two slashes are needed after the http(s):.');
                 return;
             }
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('ok');
+            res.writeHead(200, cors_headers);
+            res.end();
             return;
         }
 
